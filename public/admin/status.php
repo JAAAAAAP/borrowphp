b0ad94e7d85cfd90e904a80db9a6e977a26ea6f0
JAAAAAA <jr8o6ez@gmail.com>
2024-05-03 14:26:34 +0700

    commit 3

--- public/admin/status.php
+++ public/admin/status.php
@@ -1,5 +1,24 @@
+<div class="flex flex-col w-11/12 overflow-x-auto">
 
-
-    <div class="flex flex-col w-11/12 overflow-x-auto">
+    <table class="table text-center">
+        <thead>
+            <tr class="text-black text-base text-nowrap md:text-xl">
+                <th class="w-14">ใบที่</th>
+                <th class="w-2/4">ชื่อผู้ยืม</th>
+                <th class="">วันที่ทำรายการ</th>
+                <th class="">การอนุมัติ</th>
+            </tr>
+        </thead>
         
-    </div>
+        
+        <tbody>
+            <tr class="text-base text-nowrap md:text-xl">
+                <td class="font-bold">1</td>
+                <td>admin</td>
+                <td><?= date("d-m-Y") ?></td>
+                <td><button class="btn btn-md ">ทำการอนุมัติ</button></td>
+            </tr>   
+           
+        </tbody>
+    </table>
+</div>
